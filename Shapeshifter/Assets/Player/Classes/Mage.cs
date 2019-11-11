13c81ac7d8a32375bad21864eae152a2c6c33da2
Beau Wijkstra <beau.wijkstra@hva.nl>
2019-11-11 16:16:03 +0100

    -Mage fireball now has particles to show that it is charging -Mage now fires an explosive fireball. -Explosion particles -Explosion kills enemies (still need to change it to do exact damage)

--- Shapeshifter/Assets/Player/Classes/Mage.cs
+++ Shapeshifter/Assets/Player/Classes/Mage.cs
@@ -13,10 +13,14 @@ public class Mage : Class
     private GameObject player;
     private MonoBehaviour _mb;
 
+    public ParticleSystem particle;
+
     public Mage() : base(14, 500)
     {
         _mb = GameObject.FindObjectOfType<MonoBehaviour>();
         player = GameObject.FindGameObjectWithTag("Player");
+        particle = GameObject.FindObjectOfType<ParticleSystem>();
+        particle.Stop();
     }
 
     public override void Attack(GameObject magic, GameObject origin)
@@ -31,6 +35,10 @@ public class Mage : Class
             GameObject newArrow = GameObject.Instantiate(magic, origin.transform.position, Quaternion.identity);
             newArrow.transform.position += direction * 0.5f;
 
+            particle.transform.position = new Vector3(newArrow.transform.position.x, newArrow.transform.position.y, -1);
+            particle.Play();
+            newArrow.GetComponent<BoxCollider2D>().enabled = false;
+
             _mb.StartCoroutine(Shoot(magic, origin, direction, newArrow, 2));
             nextFireTime = Time.time + mageCooldown;
         }
@@ -41,6 +49,9 @@ public class Mage : Class
         yield return new WaitForSeconds(time);
         
         newArrow.GetComponent<Rigidbody2D>().AddForce(direction * arrowForce);
+        newArrow.GetComponent<BoxCollider2D>().enabled = true;
         player.GetComponent<Player>().canTransform = true;
+        particle.Clear();
+        particle.Stop();
     }
 }
