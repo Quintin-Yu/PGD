1a48f7cc13dbffad98e52438bca05c09fc07642b
yuq <quintin.yu@hva.nl>
2019-11-12 13:12:08 +0100

    Removed: Player movement is locked during transformation.

--- Shapeshifter/Assets/Player/Player.cs
+++ Shapeshifter/Assets/Player/Player.cs
@@ -63,8 +63,6 @@ public class Player : MonoBehaviour
     //Variables for the class switching cooldown
     private float transformCooldown;
     public float transformCooldownReset;
-    private float changeTime;
-    public float changeTimeReset;
 
     public int classIndex = 0;
 
@@ -82,7 +80,6 @@ public class Player : MonoBehaviour
         arrowReset = 2f;
 
         transformCooldownReset = 3;
-        changeTimeReset = 1;
 
         ShiftClass();
     }
@@ -111,15 +108,7 @@ public class Player : MonoBehaviour
             transformCooldown -= Time.deltaTime;
         }
 
-        if (changeTime <= 0)
-        {
-           speed = classes[classIndex].speed;
-        } else
-        {
-            changeTime -= Time.deltaTime;
-        }
-
-        if (classIndex == 0 || classIndex == 2 && changeTime <= 0)
+        if (classIndex == 0 || classIndex == 2)
         {
             if (Input.GetMouseButtonDown(0))
             {
@@ -128,7 +117,7 @@ public class Player : MonoBehaviour
         }
 
         //If classIndex == 1 (aka the archer) he has to wait untill the reload time is completed
-        if (classIndex == 1 && arrowReload <= 0 && changeTime <= 0)
+        if (classIndex == 1 && arrowReload <= 0)
         {
             if (Input.GetMouseButtonDown(0))
             {
@@ -203,12 +192,11 @@ public class Player : MonoBehaviour
 
     void ShiftClass()
     {
-        speed = 0;
+        speed = classes[classIndex].speed;
         jumpHeight = classes[classIndex].jumpHeight;
         hud.playAnimation(classIndex);
 
         transformCooldown = transformCooldownReset;
-        changeTime = changeTimeReset;
     }
 
     public IEnumerator LockMovement(float time)
