72fa3041eb8031f7dd0303eb896c3881cd255de3
yuq <quintin.yu@hva.nl>
2019-11-25 18:26:33 +0100

    Added: When the archer shoots he plays a sound

---
+++ Shapeshifter/Assets/AudioManager.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine.Audio;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    public Sound[] sounds;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        foreach(Sound s in sounds)
+        {
+            s.source = gameObject.AddComponent<AudioSource>();
+            s.source.clip = s.clip;
+
+            s.source.volume = s.volume;
+            s.source.pitch = s.pitch;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
