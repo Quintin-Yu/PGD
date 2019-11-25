af8151db57ee0f08c2fbb3af70e2c41f46023859
yuq <quintin.yu@hva.nl>
2019-11-25 22:18:00 +0100

    Changed: Shot sound now plays when the player shoots the arrow instead of when the arrow is spawned in.

--- Shapeshifter/Assets/AudioManager.cs
+++ Shapeshifter/Assets/AudioManager.cs
@@ -1,13 +1,28 @@
 ﻿using UnityEngine.Audio;
+using System;
 using UnityEngine;
 
 public class AudioManager : MonoBehaviour
 {
     public Sound[] sounds;
 
+    public static AudioManager instance;
+
     // Start is called before the first frame update
     void Awake()
     {
+        DontDestroyOnLoad(gameObject);
+
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+            return;
+        }
+
         foreach(Sound s in sounds)
         {
             s.source = gameObject.AddComponent<AudioSource>();
@@ -15,12 +30,24 @@ public class AudioManager : MonoBehaviour
 
             s.source.volume = s.volume;
             s.source.pitch = s.pitch;
+            s.source.loop = s.loop;
         }
     }
 
-    // Update is called once per frame
-    void Update()
+    void Start()
     {
-        
+        Play("LoopShot");
+    }
+
+    public void Play(string name)
+    {
+        Sound s = Array.Find(sounds, sound => sound.name == name);
+
+        if (s == null)
+        {
+            return;
+        }
+
+        s.source.Play();
     }
 }
