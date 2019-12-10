d6efdb2b3ff7668de8e059074f1f82f98a2880a1
SDMGAMMA <nicolaas.schuddeboom@upcmail.nl>
2019-12-10 15:10:02 +0100

    Gave the enemy melee the ability to climb hills and fixed in between the archer and fighter level with sound and for this ability

--- Shapeshifter/Assets/Scripts/Interactables/EnemyMelee.cs
+++ Shapeshifter/Assets/Scripts/Interactables/EnemyMelee.cs
@@ -9,6 +9,7 @@ public class EnemyMelee : Enemy
     public GameObject healthBar;
 
     public float damage = 5f;
+    public int followRange = 40;
 
     bool delayFinished = false;
     bool delayMovementFinished;
@@ -17,6 +18,7 @@ public class EnemyMelee : Enemy
     public float attackSpeedReload;
     private float attackSpeedReset;
     public float hpTimer = 2;
+    public int maxSpeed;
 
     CharacterStats myStats;
 
@@ -44,10 +46,12 @@ public class EnemyMelee : Enemy
         {
             playerMoved = false;
             delayMovementFinished = false;
+
+            rb.velocity = new Vector2(0, rb.velocity.y);
         }
 
-        if (player.transform.position.x - rb.transform.position.x >= -40 && player.transform.position.x - rb.transform.position.x <= 0 ||
-            rb.transform.position.x - player.transform.position.x >= -40 && rb.transform.position.x - player.transform.position.x <= 0)
+        if (player.transform.position.x - rb.transform.position.x >= -followRange && player.transform.position.x - rb.transform.position.x <= 0 ||
+            rb.transform.position.x - player.transform.position.x >= -followRange && rb.transform.position.x - player.transform.position.x <= 0)
         {
             if (delayFinished == false)
             {
@@ -57,43 +61,72 @@ public class EnemyMelee : Enemy
             {
                 if (delayFinished)
                 {
-                    if (player.transform.position.x - rb.transform.position.x >= -40 && player.transform.position.x - rb.transform.position.x <= 0)
+                    if (player.transform.position.x - rb.transform.position.x >= -followRange && player.transform.position.x - rb.transform.position.x <= 0)
                     {
+                        Debug.Log("inRange");
+
+                        RaycastHit2D raycastTarget = Physics2D.Raycast(new Vector2(transform.position.x - 1.1f, transform.position.y - 0.5f), -transform.right, .5f);
+                        int multiplier = 1;
+
+                        if (raycastTarget.transform != null && raycastTarget.transform.tag == "map")
+                        {
+                            multiplier = 2;
+                        }
+
                         if (playerMoved == false)
                         {
                             StartCoroutine(delayMovement(1));
                             if (delayMovementFinished == true)
                             {
-                                transform.Translate(-speed, 0f, 0f);
+                                rb.AddForce(-speed * transform.right * multiplier);
+                                
                             }
                         }
                         else
                         {
-                            transform.Translate(-speed, 0f, 0f);
+                            rb.AddForce(-speed * transform.right * multiplier);
+                            
                         }
                     }
-                    if (rb.transform.position.x - player.transform.position.x >= -40 && rb.transform.position.x - player.transform.position.x <= 0)
+                    if (rb.transform.position.x - player.transform.position.x >= -followRange && rb.transform.position.x - player.transform.position.x <= 0)
                     {
+                        RaycastHit2D raycastTarget = Physics2D.Raycast(new Vector2(transform.position.x + 1.1f, transform.position.y - 0.5f), transform.right, .5f);
+                        int multiplier = 1;
+
+                        if (raycastTarget.transform != null && raycastTarget.transform.tag == "map")
+                        {
+                            multiplier = 2;
+                        }
+
                         if (playerMoved == false)
                         {
                             StartCoroutine(delayMovement(1));
                             if (delayMovementFinished == true)
                             {
-                                transform.Translate(speed, 0f, 0f);
+                                rb.AddForce(speed * transform.right * multiplier);
                             }
                         }
                         else
                         {
-                            transform.Translate(speed, 0f, 0f);
+                            rb.AddForce(speed * transform.right * multiplier);
                         }
                     }
                 }
             }
         }
-            if (attackSpeedReset > 0)
-            {
-                attackSpeedReset -= Time.deltaTime;
-            }
+        if (attackSpeedReset > 0)
+        {
+            attackSpeedReset -= Time.deltaTime;
+        }
+
+        if (rb.velocity.x < -maxSpeed)
+        {
+            rb.velocity = new Vector2(-maxSpeed, rb.velocity.y);
+        }
+        if (rb.velocity.x > maxSpeed)
+        {
+            rb.velocity = new Vector2(maxSpeed, rb.velocity.y);
+        }
     }
     public void OnCollisionStay2D(Collision2D collision)
     { 
