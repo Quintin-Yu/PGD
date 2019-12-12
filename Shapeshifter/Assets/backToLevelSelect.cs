3ca45404ec61ff23d58c97c44feb9997d84db5de
yuq <quintin.yu@hva.nl>
2019-12-12 10:12:32 +0100

    Used wall to go back to level selector

---
+++ Shapeshifter/Assets/backToLevelSelect.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class backToLevelSelect : MonoBehaviour
+{
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag.Equals("Player"))
+        {
+            SceneManager.LoadScene("Level Selector");
+        }
+    }
+}
