87b8baf98e384ef16e010c176cb707e9f25b8e9c
yuq <quintin.yu@hva.nl>
2019-12-14 17:29:50 +0100

    Removed unnecesarry scripts. Changed all scene change scripts to one script called sceneManagement

---
+++ Shapeshifter/Assets/Scripts/SceneManagement.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneManagement : MonoBehaviour
+{
+    public void LevelSelector()
+    {
+        SceneManager.LoadScene("Level Selector");
+    }
+
+    public void FighterLevel()
+    {
+        SceneManager.LoadScene("FighterLevel");
+    }
+
+    public void MageLevel()
+    {
+        SceneManager.LoadScene("MageLevel");
+    }
+
+    public void ArcherLevel()
+    {
+        SceneManager.LoadScene("ArcherLevel");
+    }
+
+    public void MainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public void GameOver()
+    {
+        SceneManager.LoadScene("Gane Over");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
