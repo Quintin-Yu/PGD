6bb3a02cfecfcee952671b91ffeb94c05318882b
SDMGAMMA <nicolaas.schuddeboom@upcmail.nl>
2020-01-07 18:52:20 +0100

    Fixed bug where arrows don't have angle when shot

--- Shapeshifter/Assets/Scripts/Projectiles.cs
+++ Shapeshifter/Assets/Scripts/Projectiles.cs
@@ -5,11 +5,20 @@ using UnityEngine;
 [SerializeField]
 public class Projectiles : MonoBehaviour
 {
+    Rigidbody2D rb;
     public float projectileLifeTime;
 
     private void Update()
     {
+        rb = GetComponent<Rigidbody2D>();
         Destroy(gameObject, projectileLifeTime);
+    }
+
+    private void FixedUpdate()
+    {
+        float rad = Mathf.Atan(rb.velocity.y / rb.velocity.x);
+        float deg = rad * 180 / Mathf.PI;
+        transform.eulerAngles = new Vector3(0, 0, deg);
     }
 
     public virtual void OnTriggerEnter2D(Collider2D other)
